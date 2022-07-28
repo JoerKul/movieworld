f269bc7f6459bfe02210c123ae64cc92c48350a8
Kullrich <joerg.kullrich@siemens.com>
2022-07-28 16:31:52 +0200

    add styles and icon first api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+// Here is your key: dfd2f984
+//OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=dfd2f984
+
+const API_URL = "http://www.omdbapi.com?apikey=dfd2f984";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("The Matrix");
+  }, []);
+
   return (
-    <div>
-      <h1>App</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for a movie"
+          value="Superman"
+          onChange={() => {}}
+        />
+      </div>
     </div>
   );
 };
