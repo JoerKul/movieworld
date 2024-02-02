f40cd4fd2a512e00dba98086c3d80cab01342a6f
Jörg Kullrich <joerg.kullrich@siemens.com>
2024-02-02 08:25:42 +0100

    chore: update readme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import MovieCard from "./MovieCard";
+import React, { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
 
-import "./App.css";
-import SearchIcon from "./search.svg";
+import './App.css';
+import SearchIcon from './search.svg';
 
 // Here is your key: dfd2f984
 //OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=dfd2f984
 
-const API_URL = "http://www.omdbapi.com?apikey=dfd2f984";
+const API_URL = 'http://www.omdbapi.com?apikey=dfd2f984';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -23,30 +23,30 @@ const App = () => {
   // }, []);
 
   return (
-    <div className="app">
-      <h2>MovieLand</h2>
+    <div className='app'>
+      <h1>MovieLand</h1>
 
-      <div className="search">
+      <div className='search'>
         <input
-          type="text"
-          placeholder="Search for a movie"
+          type='text'
+          placeholder='Search for a movie'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           src={SearchIcon}
-          alt="Search"
+          alt='Search'
           onClick={() => searchMovies(searchTerm)}
         />
       </div>
       {movies?.length > 0 ? (
-        <div className="container">
+        <div className='container'>
           {movies.map((movie) => (
             <MovieCard key={movie.imdbID} movie={movie} />
           ))}
         </div>
       ) : (
-        <div className="empty">
+        <div className='empty'>
           <h2>No movies found</h2>
         </div>
       )}
